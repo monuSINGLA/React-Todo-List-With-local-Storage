43822ec6999b3b832a853981d5c4c42da2be2844
Yogesh Singla <monusingla104@gmail.com>
2024-02-19 16:27:23 +0530

    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { useTodo } from '../context';
+
+function TodoItem({ todo }) {
+    const [isTodoUpdating, setIsTodoUpdating] = useState(false)
+    const [todoMessege , setTodoMessege ] = useState(todo.todo)
+
+    const {updateTodo, deleteTodo, toggleComplete} = useTodo()
+
+    const editTodo = ()=>{
+        updateTodo(todo.id, {...todo, todo: todoMessege})
+        setIsTodoUpdating(false)
+    }
+    const toggleCompleted = ()=> {
+        toggleComplete(todo.id)
+    }
+
+    return (
+        <div
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+            }`}
+        >
+            <input
+                type="checkbox"
+                className="cursor-pointer"
+                checked={todo.completed}
+                onChange={toggleCompleted}
+            />
+            <input
+                type="text"
+                className={`border outline-none w-full bg-transparent rounded-lg ${
+                    isTodoUpdating ? "border-black/10 px-2" : "border-transparent"
+                } ${todo.completed ? "line-through" : ""}`}
+                value={todoMessege}
+                onChange={(e) => setTodoMessege(e.target.value)}
+                readOnly={!isTodoUpdating}
+            />
+            {/* Edit, Save Button */}
+            <button
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                onClick={() => {
+                    if (todo.completed) return;
+
+                    if (isTodoUpdating) {
+                        editTodo();
+                    } else setIsTodoUpdating((prev) => !prev);
+                }}
+                disabled={todo.completed}
+            >
+                {isTodoUpdating ? "📁" : "✏️"}
+            </button>
+            {/* Delete Todo Button */}
+            <button
+                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+                onClick={() => deleteTodo(todo.id)}
+            >
+                ❌
+            </button>
+        </div>
+    );
+}
+
+export default TodoItem;
+
